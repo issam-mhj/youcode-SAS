b05856ca4d2c3c8fd938d0db60854d695ad668aa
unknown <issammahtaj02@gmail.com>
2024-09-11 15:22:12 +0200

    les boucles

---
+++ Day3/Loops/challenge_1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int nombre;
+    printf("entrer a nombre : ");
+    scanf("%d",&nombre);
+
+    for (int i=1 ; i<=10 ; i++){
+        printf("%d * %d = %d \n",nombre,i,nombre*i);
+    }
+}
