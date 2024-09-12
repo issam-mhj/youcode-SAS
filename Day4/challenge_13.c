fc719c27a880e89fd5727c1086469886133d0e40
unknown <issammahtaj02@gmail.com>
2024-09-12 15:58:06 +0200

    les tableau

---
+++ Day4/challenge_13.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int nombre , i = 0;
+    printf("entrer le nombre d'elements: ");
+    scanf("%d", &nombre);
+    int table[nombre];
+    for (i = 0; i < nombre; i++)
+    {
+        printf("entrer l'element %d :", i + 1);
+        scanf("%d",&table[i]);
+    }
+
+    printf("les element impairs de votre tableaux: \n");
+    
+    for (i = 0; i < nombre; i++)
+    {
+        if (table[i] %2 != 0)
+            printf("%d\n", table[i]);
+    }
+
+
+    return 0;
+}
