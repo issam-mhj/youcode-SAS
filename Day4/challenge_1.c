55d057dcff6c0d158b49660f2f64472ed0834a5f
unknown <issammahtaj02@gmail.com>
2024-09-12 12:52:48 +0200

    les tableau

--- Day4/challenge_1.c
+++ Day4/challenge_1.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-int main() {
-int num[5]={1,2,3,4,5};
-
-    for(int i=0; i<5;i++ ){
-        printf("%d\n",num[i]);
+int main()
+{
+    int num[5] = {1, 2, 3, 4, 5};
+    int i;
+    for (i = 0; i < 5; i++)
+    {
+        printf("%d\n", num[i]);
     }
 
     return 0;
