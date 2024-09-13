adaa22f4c769c47cffdb7d1fd25c94d30d436d17
unknown <issammahtaj02@gmail.com>
2024-09-13 16:42:17 +0200

    les fonctions

---
+++ Day5/challenge_6.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+int fibo(int num){
+    int i,x,x1,x2;
+    x=0;
+    x1=1;
+    for(i=0;i<num;i++){
+        x2=x1;
+        x1=x+x1;
+        x=x2;
+    }
+    return x;
+}
+
+
+
+
+int main() {
+    int num;
+    printf("entrez a nombre: ");
+    scanf("%d",&num);
+    printf("le terme de Fibonacci demande %d",fibo(num));
+
+    return 0;
+}
