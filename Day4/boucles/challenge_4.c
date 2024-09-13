b529719b295c62ff820240c10e8821146cb8cc8c
unknown <issammahtaj02@gmail.com>
2024-09-13 11:17:47 +0200

    les boucles

---
+++ Day4/boucles/challenge_4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int nombre,reverse=0;
+    printf("entre un nombre entier: ");
+    scanf("%d",&nombre);
+
+    for(int i=0;nombre>0;i++){
+        reverse = (reverse*10) + (nombre % 10);
+        nombre = nombre /10 ;  
+    }
+
+    printf("le nombre reversed = %d",reverse);
+    
+    return 0;
+}
+
